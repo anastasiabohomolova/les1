cf53c4b88d4eeed4d109b1537d3ada64d6a46c41
Anastasia <136612325+anastasiabohomolova@users.noreply.github.com>
2023-06-15 09:26:05 +0300

    1.2

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-a = 'Привіт '
-b = input('Введіть ваше ім`я: ')
-h = ' '
-c = input('Введіть ваше прізвище: ')
-print(a + b + h + c + '!')
-d = input('Введіть свій вік : ')
-print(a + b + h + c + h + d)
+#a = input('')
+#b = int(a)
+#print(type(b))
+#c = id(b)
+#print(c)
+
+#d = '3', '5', '6'
+#g = int(d)
+#print(g)
+
+l = input('')
+o = input('')
+i = input('')
+a = l + o + i
+b = int(a)
+print(type(b))
+c = id(b)
+print(c)
