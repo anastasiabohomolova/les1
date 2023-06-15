c786807e38919c124f30e05e0fd92a4927439da0
Anastasia <136612325+anastasiabohomolova@users.noreply.github.com>
2023-06-15 09:39:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-#a = input('')
-#b = int(a)
-#print(type(b))
-#c = id(b)
-#print(c)
-
-#d = '3', '5', '6'
-#g = int(d)
-#print(g)
-
-l = input('')
-o = input('')
-i = input('')
-a = l + o + i
-b = int(a)
-print(type(b))
-c = id(b)
-print(c)
+a = 'Привіт '
+b = input('Введіть ваше ім`я: ')
+h = ' '
+c = input('Введіть ваше прізвище: ')
+print(a + b + h + c + '!')
+d = input('Введіть свій вік : ')
+print(a + b + h + c + h + d)
